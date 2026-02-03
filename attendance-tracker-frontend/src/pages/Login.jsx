7359409246703941360016a71b7eecba6af1a2f1
rohitpm2004 <kirsagarmenaka@gmail.com>
2026-02-03 14:18:48 +0530

    Update login API endpoint to production URL

--- attendance-tracker-frontend/src/pages/Login.jsx
+++ attendance-tracker-frontend/src/pages/Login.jsx
@@ -15,7 +15,7 @@ function Login() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/teachers/login",
+        "https://online-attendence-tracker-1.onrender.com/api/teachers/login",
         { email, password }
       );
 
